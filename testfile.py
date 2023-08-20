6dc2a0a2df2889123d909d9ad6712ea5c4dd007f
rishi1212 <me.rishiverma@gmail.com>
2023-08-20 22:52:37 +0530

    changes final pivot index question

--- testfile.py
+++ testfile.py
@@ -1,1 +1,19 @@
 print ("Hello world")
+
+aList = [1,7,3,6,5,6]
+
+lengthOfArray = len(aList)
+
+
+
+
+print("the solution")
+for i in range(0,lengthOfArray):
+    # print (aList[0:i] )
+    # print("sum is equal to: "+ str(sum(aList[0:i])))
+    # print (aList[i+1:lengthOfArray] )
+    # print("sum is equal to: " + str(sum(aList[i:lengthOfArray])))
+    if(sum(aList[0:i]) == sum(aList[i+1:lengthOfArray])):
+        print (i)
+    else:
+        print ("-1")
